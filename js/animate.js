d4473f453de20c3fde6faa19eb32b8130fd14204
JoacoGraffigna <joacograffigna@gmail.com>
2023-06-09 13:53:59 -0300

    c

--- js/animate.js
+++ js/animate.js
@@ -1,33 +1,38 @@
-//$(() => {
+// $(selector).acción()
+
+// $("h1").hide()
+
+// $(document).ready(() => {
+$(() => {
     // Selectores:
     // id="el-1" => "#el-1"
     // class="el-1" => ".el-1"
     // $("#el-1").hide()
 
-  //  $(".primerboton").click(() => {
+    $(".hide-btn").click(() => {
         // $("h1").hide()
-      //  $(".rd-navbar container rd-navbar-floated rd-navbar-dark rd-navbar--is-touch rd-navbar-original rd-navbar-fixed active").fadeOut()
-    //})
-    //$(".show-btn").click(() => {
+        $("h1").fadeOut()
+    })
+    $(".show-btn").click(() => {
         // $("h1").show()
-    //    $("h1").fadeIn()
-    //})
+        $("h1").fadeIn()
+    })
 
-    //$("li").dblclick(() => {
-    //    $("h1").css({ color: "red" })
-    //})
+    $("li").dblclick(() => {
+        $("h1").css({ color: "red" })
+    })
 
-    //$(".new-element").click(() => {
+    $(".new-element").click(() => {
         // $("ul").append("<li>New Element</li>")
-      //  $("ul").prepend("<li>New Element</li>")
-    //})
+        $("ul").prepend("<li>New Element</li>")
+    })
 
-    //$("li").mouseenter((elem) => {
-      //  elem.target.style.color = "blue"
-    //})
+    $("li").mouseenter((elem) => {
+        elem.target.style.color = "blue"
+    })
 
-    //$("li").mouseleave(elem => {
-      //  elem.target.style.color = "black"
-    //})
+    $("li").mouseleave(elem => {
+        elem.target.style.color = "black"
+    })
 
-//}
+})
