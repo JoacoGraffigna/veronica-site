9159b12ef8cfbca450755bbabb9e1556d22406db
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-06-09 13:29:41 -0300

    fix menu toggle

--- js/animate.js
+++ js/animate.js
@@ -1,33 +1,5 @@
-//$(() => {
-    // Selectores:
-    // id="el-1" => "#el-1"
-    // class="el-1" => ".el-1"
-    // $("#el-1").hide()
-
-  //  $(".primerboton").click(() => {
-        // $("h1").hide()
-      //  $(".rd-navbar container rd-navbar-floated rd-navbar-dark rd-navbar--is-touch rd-navbar-original rd-navbar-fixed active").fadeOut()
-    //})
-    //$(".show-btn").click(() => {
-        // $("h1").show()
-    //    $("h1").fadeIn()
-    //})
-
-    //$("li").dblclick(() => {
-    //    $("h1").css({ color: "red" })
-    //})
-
-    //$(".new-element").click(() => {
-        // $("ul").append("<li>New Element</li>")
-      //  $("ul").prepend("<li>New Element</li>")
-    //})
-
-    //$("li").mouseenter((elem) => {
-      //  elem.target.style.color = "blue"
-    //})
-
-    //$("li").mouseleave(elem => {
-      //  elem.target.style.color = "black"
-    //})
-
-//}
+$( document ).ready(function() {
+  $(".rd-navbar-nav-wrap li").click(function(){
+    $(".rd-navbar-nav-wrap").removeClass("active");
+  });
+});
