8f7b6c2db8b90d3ba9187e88993300ee950f962b
JoacoGraffigna <joacograffigna@gmail.com>
2023-07-16 16:27:02 -0300

    c

--- js/animate.js
+++ js/animate.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
   $(".rd-navbar-nav-wrap li").click(function(){
     $(".rd-navbar-nav-wrap").removeClass("active");
+    $(".rd-navbar-toggle").removeClass("active");
   });
 });
+
+
